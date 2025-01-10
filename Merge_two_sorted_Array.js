0667771e93f0cdbab50a74e6e9479a23f1ed5b0e
Mohammad Ali <123582111+MohammadAliiter@users.noreply.github.com>
2025-01-10 15:02:06 +0530

    Add[MergeTwoSortedArray]

---
+++ Merge_two_sorted_Array.js
@@ -0,0 +1,35 @@
+function mergeSortedArrays(arr1, arr2) {
+    let mergedArray = [];
+    let i = 0, j = 0;
+
+    // Traverse both arrays and compare elements
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push(arr1[i]);
+            i++;
+        } else {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+    }
+
+    // Add remaining elements from arr1
+    while (i < arr1.length) {
+        mergedArray.push(arr1[i]);
+        i++;
+    }
+
+    // Add remaining elements from arr2
+    while (j < arr2.length) {
+        mergedArray.push(arr2[j]);
+        j++;
+    }
+
+    return mergedArray;
+}
+
+// Example usage
+const array1 = [1, 3, 5, 7];
+const array2 = [2, 4, 6, 8];
+const result = mergeSortedArrays(array1, array2);
+console.log("Merged Array:", result);
