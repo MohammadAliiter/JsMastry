a70747f2820c10f422ac556c50f52cff84ffa7c0
Mohammad Ali <123582111+MohammadAliiter@users.noreply.github.com>
2025-01-14 13:23:04 +0530

    Add[TwoSum,O(n)]

---
+++ TwoSum.js
@@ -0,0 +1,20 @@
+const TwoSum = (nums, target) =>{
+
+    const map = new Map();
+       
+for(let i = 0; i < nums.length; i++){
+       
+    const complement = target - nums[i];
+
+    if(map.has(complement)){
+        return [map.get(complement), i];
+    }
+
+    map.set(nums[i], i);
+
+}
+}
+
+const arr = [2,7,11,15];
+const target = 9;
+console.log(TwoSum(arr,target));
