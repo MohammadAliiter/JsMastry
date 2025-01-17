4baa0ee1dd02b705bf49e54f0ce7bb5b6a375b9b
Mohammad Ali <123582111+MohammadAliiter@users.noreply.github.com>
2025-01-17 12:40:13 +0530

    ADD[typeScriptsConcepts]

---
+++ Interface_and_Alias.ts
@@ -0,0 +1,24 @@
+
+interface Person {
+    name: String,
+    age: Number
+}
+
+const person: Person = {
+    name: "Mohammad",
+    age:25,
+}
+console.log(person);
+
+
+type Students = {
+    name: String,
+    age: Number,
+}
+
+const students: Students = {
+    name:"Mohammad",
+    age:25,
+}
+
+console.log(students);
